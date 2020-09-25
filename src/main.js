5218ed437b0fd0efb9aaa958be94e82511481e28
pavelcity <44302106+pavelcity@users.noreply.github.com>
2020-09-25 07:46:28 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,13 +13,13 @@ new Vue({
 	store,
 	created () {
 		fb.initializeApp ({
-		apiKey: "AIzaSyCSQEVo-qXxKLmAVpXH-j0K-5Ho4nMV5xI",
-    authDomain: "dentasev.firebaseapp.com",
-    databaseURL: "https://dentasev.firebaseio.com",
+		apiKey: "???",
+    authDomain: "????",
+    databaseURL: "???",
     projectId: "dentasev",
-    storageBucket: "dentasev.appspot.com",
-    messagingSenderId: "466206880259",
-    appId: "1:466206880259:web:1cded90be59e727aab5436"
+    storageBucket: "???",
+    messagingSenderId: "???",
+    appId: "???"
 		})
 	}
 }).$mount('#app')
